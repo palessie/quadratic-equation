14afeddf89400dd64ffa471cc911ac76fa42e91c
Palesse <1869680@gmail.com>
2018-09-23 22:37:40 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let arr = equation.split(' '),
+      a   = parseFloat(arr[0]),
+      b   = parseFloat(arr[arr.lastIndexOf('*') - 2] + arr[arr.lastIndexOf('*') - 1]),
+      c   = parseFloat(arr[arr.length - 2] + arr[arr.length - 1]),
+
+      D   = Math.pow(b, 2) - 4 * a * c,
+      x1  = Math.round((-b - Math.sqrt(D)) / (2 * a)),
+      x2  = Math.round((-b + Math.sqrt(D)) / (2 * a));
+
+  if(x1 > x2) {
+    return [x2, x1];
+  }
+    return [x1,x2];
 }
